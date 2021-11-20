4241d7f7473164b65c6cc6bfb9fb741dae1f959c
(004_17) Максимчук Иван <004_17@kep.local>
2021-11-20 10:52:21 +0300

    Edit page

---
+++ script.js
@@ -0,0 +1,17 @@
+new Vue({
+	el: '#app',
+	data: {
+		navbar_items: [
+			{title: 'Список лиг', href:'#'},
+			{title: 'Список команд', href:'#'},
+			{title: 'Календарь лиги', href:'#'},
+			{title: 'Календарь одной команды', href:'#'},
+		]
+	},
+	methods: {
+
+	},
+	computed: {
+
+	}
+});
