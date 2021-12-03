72ef42c458994bca87aa3217055a55bc94d40937
Ivan Maksimchuk <baixa2003@gmail.com>
2021-12-03 19:00:35 +0300

    Edit js file

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ new Vue({
 	el: '#app',
 	data: {
 		navbar_items: [
-			{title: 'Список лиг', href:'#'},
-			{title: 'Список команд', href:'#'},
-			{title: 'Календарь лиги', href:'#'},
-			{title: 'Календарь одной команды', href:'#'},
+			{title: 'Список лиг', href:'#', onclick:'listLeguesClick()'},
+			{title: 'Список команд', href:'#', onclick:'listTeamsClick()'},
+			{title: 'Календарь лиги', href:'#', onclick:'listGamesLeagueClick()'},
+			{title: 'Календарь одной команды', href:'#', onclick:'listGamesTeamClick()'},
 		]
 	},
 	methods: {
@@ -16,11 +16,35 @@ new Vue({
 	}
 });
 
+
+var teams = []
+
+
+
+
 $.ajax({
   headers: { 'X-Auth-Token': 'f3f3dad5142049a481c69a81fdda3d0d' },
-  url: "http://api.football-data.org/v2/competitions/PL/matches",
+  url: "https://api.football-data.org/v2/teams",
   dataType: 'json',
   type: 'GET',
 }).done(function(response) {
-  console.log(response);
+	for(let i = 0; i < 10; i++) {
+  	teams += (response['teams'][i]);
+  	console.log(response['teams'][i]);
+  }
 });
+
+
+function listTeamsClick() {
+	var main = document.getElementById("col");
+	var resultHTML = "";
+	for(let i = 0; i < teams.length; i++) {
+		resultHTML  = resultHTML + '<a onclick="teamGames()">' + teams[i]['name'] + '</a>';
+		console.log(teams);
+	}
+	var el = document.getElementById('col');
+	el.innerHTML = resultHTML;
+}
+function teamGames() {
+
+}
